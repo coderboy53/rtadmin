c436a2ed6adcd0f7d2cac9a6b782b45a9c9a3ae7
Soham Mitra <mitra.soham53@gmail.com>
2023-06-11 13:59:14 +0530

    finalized hotel and login functionality jsx with basic styles

--- client/src/Hotel.jsx
+++ client/src/Hotel.jsx
@@ -1,10 +1,63 @@
-export default function Hotel() {
+import './css/hotel.css';
+import { useState } from 'react';
+const Hotel = () => {
+    const [enabled, setEnabled] = useState(false);
     return (
        <>
             <h2>Add New Hotel</h2>
             <form action="">
-                
+                <div className="left">
+                    <label htmlFor="hName">Hotel Name:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="hName" id="hName" /><br />
+                    <label htmlFor="hadd">Hotel Address:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="hAdd" id="hAdd" /><br />
+                    <label htmlFor="city">City:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="city" id="city" /><br />
+                    <label htmlFor="state">State:</label>
+                    <input type="text" name="state" id="state" /><br />
+                    <label htmlFor="latitude">Latitude:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="latitude" id="latitude" /><br />
+                    <label htmlFor="rCount">Room Count:<span style={{color:'red'}}>*</span></label>
+                    <input type="number" name="rCount" id="rCount" /><br />
+                </div>
+                <div className="right">
+                    <label htmlFor="country">Country:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="country" id="country" /><br />
+                    <label htmlFor="starRate">Star Rating:<span style={{color:'red'}}>*</span></label>
+                    <select name="starRate" id="starRate">
+                        <option value="null" disabled selected>Select</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select><br />
+                    <label htmlFor="hSortName">Hotel Sort Name:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="hSortName" id="hSortName" /><br />
+                    <label htmlFor="zipCode">Zip Code:</label>
+                    <input type="text" name="zipCode" id="zipCode" /><br />
+                    <label htmlFor="telNo">Telephone No:</label>
+                    <input type="text" name="telNo" id="telNo" /><br />
+                    <label htmlFor="longitude">Longitude:<span style={{color:'red'}}>*</span></label>
+                    <input type="text" name="longitude" id="longitude" /><br />
+                </div>
+                <label htmlFor="propId">Property ID:<span style={{color:'red'}}>*</span></label><br /><br />
+                <input type="radio" name="radio" id="hCode" onChange={()=>{if(enabled) setEnabled(false)}}/>Same as hotel code <br />
+                <input type="radio" name="radio" id="value" onChange={()=>{setEnabled(!enabled)}}/>Value<span style={{color:'red'}}>*</span> <br />
+                {
+                    enabled? (
+                        <input type="text" name="propId" id="propId" size={50}/>
+                    ):
+                    (
+                        <input type="text" name="propId" id="propId" size={50} disabled style={{backgroundColor:'#bcbcbc'}}/>
+                    )
+                } <br />
+                <button>Save and Close</button>
+                <button>Save and Add</button>
+                <button>Reset</button>
             </form>
        </> 
     );
 }
+
+export default Hotel;
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,16 +1,18 @@
 import './css/login.css'
 const Login = () => {
     return (
-        <div id="login-tab">
-            <img src="" alt="" srcset="" />
-            <form action="">
-                <h3>Sign in to RTAdmin</h3>
-                <table>
-                    <tr><td><label htmlFor="uname">Username</label></td><td><input type="text" name="uname" id="uname" /></td></tr>
-                    <tr><td><label htmlFor="pass">Password</label></td><td><input type="password" name="pass" id="pass" /></td></tr>            
-                </table>
-                <input type="submit" value="Login" id="loginSubmit" />
-            </form>
+        <div id='login'>
+            <div id="login-tab">
+                <img src="" alt="" srcset="" />
+                <form action="">
+                    <h3>Sign in to RTAdmin</h3>
+                    <table>
+                        <tr><td><label htmlFor="uname">Username</label></td><td><input type="text" name="uname" id="uname" /></td></tr>
+                        <tr><td><label htmlFor="pass">Password</label></td><td><input type="password" name="pass" id="pass" /></td></tr>
+                    </table>
+                    <input type="submit" value="Login" id="loginSubmit" />
+                </form>
+            </div>
         </div>
     );
 }
