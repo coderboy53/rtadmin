bdbc27eff2d39cb6ac62c0d141770f2d8396b3e7
Soham Mitra <mitra.soham53@gmail.com>
2023-06-13 00:15:46 +0530

    completed initial login route request and response handling, integration with database remaining and further jwt authentication required

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+const crypto = require('crypto')
 const express = require('express');
-const cors = require('cors')
-const ADODB = require('node-adodb');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+// const ADODB = require('node-adodb');
 const app = express();
-const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=../rtadmin-db.accdb;')
+// const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=../rtadmin-db.accdb;')
+
+const creds = {user: 'soham.mitra53', pass: crypto.createHash('sha512').update('soham2001').digest('hex')};
 
 app.use(cors());
 app.use(express.json());
@@ -14,4 +18,21 @@ app.listen(port, () => {
 
 app.get("/api", (req, res) => {
     res.json({"users":["userOne","userTwo","userThree"]})
+});
+
+app.post("/api/login", (req, res) => {
+    const uname = req.body.user;
+    const passwd = req.body.pass;
+    if(uname===creds.user && passwd===creds.pass)
+    {
+        return res.status(200).json({message: 'Logged in'});
+    }
+    else
+    {
+        return res.status(400).json({message: 'Incorrect credentials'});
+    }
+});
+
+app.post("/api/hotel/add", (req, res) => {
+
 });
