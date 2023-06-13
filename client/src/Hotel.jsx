176bd8a1ab8a99115a7481c5bacdf849fd3c179b
Soham Mitra <mitra.soham53@gmail.com>
2023-06-13 23:25:30 +0530

    fixed the usage of state to add form data

--- client/src/Hotel.jsx
+++ client/src/Hotel.jsx
@@ -3,26 +3,20 @@ import { useState } from 'react';
 
 const Hotel = () => {
     const [enabled, setEnabled] = useState(false);
-    const [hotel, setHotel] = useState({
-        hotelName: '',
-        hotelAddr: '',
-        city: '',
-        state: '',
-        latitude: '',
-        room_count: 0,
-        country: '',
-        star_rating: '',
-        hotelSortName: '',
-        zipcode: '',
-        teleNo: '',
-        longitute: '',
-        propertyId: '',
-    });
+    const [hotel, setHotel] = useState({});
 
-    const handleUpdate = () => {
-
+    const handleChange = (event) => {
+        let name = event.target.name;
+        const entry = {};
+        entry[name] = event.target.value;
+        setHotel({...hotel, ...entry});
+    }
+    const handleUpdate = (event) => {
+        event.preventDefault();
+        console.log(hotel);
     };
-    const handleAdd = () => {
+    const handleAdd = (event) => {
+        event.preventDefault();
 
     };
     return (
@@ -31,23 +25,23 @@ const Hotel = () => {
             <form>
                 <div className="left">
                     <label htmlFor="hName">Hotel Name:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="hName" id="hName" required onChange={(event)=>{setHotel(hotel.hotelName = event.target.value)}}/><br />
+                    <input type="text" name="hName" id="hName" required onChange={handleChange}/><br />
                     <label htmlFor="hadd">Hotel Address:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="hAdd" id="hAdd" required onChange={(event) => {setHotel(hotel.hotelAddr=event.target.value)}}/><br />
+                    <input type="text" name="hAdd" id="hAdd" required onChange={handleChange}/><br />
                     <label htmlFor="city">City:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="city" id="city" required onChange={(event) => {setHotel(hotel.city = event.target.value)}}/><br />
+                    <input type="text" name="city" id="city" required onChange={handleChange}/><br />
                     <label htmlFor="state">State:</label>
-                    <input type="text" name="state" id="state" onChange={(event)=>{setHotel(hotel.state = event.target.value)}}/><br />
+                    <input type="text" name="state" id="state" onChange={handleChange}/><br />
                     <label htmlFor="latitude">Latitude:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="latitude" id="latitude" required onChange={(event) => {setHotel(hotel.latitude = event.target.value)}}/><br />
+                    <input type="text" name="latitude" id="latitude" required onChange={handleChange}/><br />
                     <label htmlFor="rCount">Room Count:<span style={{color:'red'}}>*</span></label>
-                    <input type="number" name="rCount" id="rCount" required onChange={(event) => {hotel.room_count = event.target.value}}/><br />
+                    <input type="number" name="rCount" id="rCount" required onChange={handleChange}/><br />
                 </div>
                 <div className="right">
                     <label htmlFor="country">Country:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="country" id="country" required onChange={(event) => {setHotel(hotel.country = event.target.value)}}/><br />
-                    <label htmlFor="starRate">Star Rating:<span style={{color:'red'}}>*</span></label>
-                    <select name="starRate" id="starRate" required onChange={(event) => {setHotel(hotel.star_rating = event.target.value)}}>
+                    <input type="text" name="country" id="country" required onChange={handleChange}/><br />
+                    <label htmlFor="starRate">Star Rating:<span style={{color:'red'}}>*</span></label><br />
+                    <select name="starRate" id="starRate" required onChange={handleChange}>
                         <option value="null" disabled selected>Select</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -56,20 +50,20 @@ const Hotel = () => {
                         <option value="5">5</option>
                     </select><br />
                     <label htmlFor="hSortName">Hotel Sort Name:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="hSortName" id="hSortName" required onChange={(event) => {setHotel(hotel.hotelSortName = event.target.value)}}/><br />
+                    <input type="text" name="hSortName" id="hSortName" required onChange={handleChange}/><br />
                     <label htmlFor="zipCode">Zip Code:</label>
-                    <input type="text" name="zipCode" id="zipCode" onChange={(event) => {setHotel(hotel.zipcode = event.target.value)}}/><br />
+                    <input type="text" name="zipCode" id="zipCode" onChange={handleChange}/><br />
                     <label htmlFor="telNo">Telephone No:</label>
-                    <input type="text" name="telNo" id="telNo" onChange={(event) => {setHotel(hotel.teleNo = event.target.value)}}/><br />
+                    <input type="text" name="telNo" id="telNo" onChange={handleChange}/><br />
                     <label htmlFor="longitude">Longitude:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="longitude" id="longitude" required onChange={(event) => {setHotel(hotel.longitute = event.target.value)}}/><br />
+                    <input type="text" name="longitude" id="longitude" required onChange={handleChange}/><br />
                 </div>
                 <label htmlFor="propId">Property ID:<span style={{color:'red'}}>*</span></label><br /><br />
                 <input type="radio" name="radio" id="hCode" onChange={()=>{if(enabled) setEnabled(false)}}/>Same as hotel code <br />
                 <input type="radio" name="radio" id="value" onChange={()=>{setEnabled(!enabled)}}/>Value<span style={{color:'red'}}>*</span> <br />
                 {
                     enabled? (
-                        <input type="text" name="propId" id="propId" size={50}/>
+                        <input type="text" name="propId" id="propId" size={50} required/>
                     ):
                     (
                         <input type="text" name="propId" id="propId" size={50} disabled style={{backgroundColor:'#bcbcbc'}}/>
