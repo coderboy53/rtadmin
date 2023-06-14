0da013f82677ea1c16e8937e9ace6c7485b69471
coderboy53 <mitra.soham53@gmail.com>
2023-06-14 23:21:22 +0530

    added the database in server folder, fixed integration of node-adodb module to connect to db

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
+'use strict';
 const crypto = require('crypto')
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-// const ADODB = require('node-adodb');
+const ADODB = require('node-adodb');
+const { error } = require('console');
 const app = express();
-// const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=../rtadmin-db.accdb;')
-
-const creds = {user: 'soham.mitra53', pass: crypto.createHash('sha512').update('soham2001').digest('hex')};
-
+const connection = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=rtadmin-db.accdb;Persist Security Info=False;',true)
 app.use(cors());
 app.use(express.json());
 
@@ -23,22 +22,33 @@ app.get("/api", (req, res) => {
 app.post("/api/login", (req, res) => {
     const uname = req.body.user;
     const passwd = req.body.pass;
-    if(uname===creds.user && passwd===creds.pass)
-    {
-        return res.status(200).json({message: 'Logged in'});
-    }
-    else
-    {
-        return res.status(400).json({message: 'Incorrect credentials'});
-    }
+    
 });
 
 app.post("/api/hotel/add", (req, res) => {
-    console.log(req.body);
-    return res.status(200).json({message: 'Hotel added successfully'});
+    connection.execute('INSERT INTO Users VALUES (3, "xyzmitra", "sohamhello")')
+    .then(data => {
+        console.log(JSON.stringify(data, null, 2));
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 });
+
+app.get("/api/test", async (req, res) => {
+    try{
+        const users = await connection.query('SELECT * FROM Users');
+        console.log(JSON.stringify(users, null, 2));
+    }
+    catch(error)
+    {
+        console.error(error);
+    }
+})
 
 app.put('/api/hotel/update', (req, res) => {
     
 })
+
+// efd9259cbd96593e6cb779947f8996b7271ddcd13e44f8afda8ae6bdf37fc9cb22911f38d0057a745e1b820cb1f0ab7728fd77d3a83c5547f14a54bbaec081fb 
