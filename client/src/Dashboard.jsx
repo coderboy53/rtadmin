12e4d92f674df21b43d1bd3f2e090aa3aca5033f
coderboy53 <mitra.soham53@gmail.com>
2023-06-18 17:46:06 +0530

    initial work on dashboard that loads unauthorized page when jwt is not set or expired, further work needed

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,7 +1,19 @@
+import Unauthorized from "./assets/Unauthorized";
+import { useEffect } from "react";
 const Dashboard = () => {
-    return (
-        <h1>Dashboard</h1>
-    );
+    const isJwtSet = Cookies.get('jwt') !== undefined;
+    if(isJwtSet)
+    {
+        return <Unauthorized />
+    }
+    else
+    {
+        return (
+            <>
+                <h1>Dashboard</h1>
+            </>
+        );
+    }
 }
 
 export default Dashboard;
