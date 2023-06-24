3b8cca2ae8cc10b3223c6551c59c038cad95adcc
coderboy53 <mitra.soham53@gmail.com>
2023-06-25 02:37:18 +0530

    added input validation to login

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -54,8 +54,8 @@ const Login = () => {
                     <h3>Sign in to RTAdmin</h3>
                     <table>
                         <tbody>
-                        <tr><td><label htmlFor="uname">Username</label></td><td><input type="text" name="uname" id="uname" onChange={(event)=>{setUsername(event.target.value)}}/></td></tr>
-                        <tr><td><label htmlFor="pass">Password</label></td><td><input type="password" name="pass" id="pass" onChange={(event)=>{setPassword(SHA512(event.target.value).toString())}}/></td></tr>
+                        <tr><td><label htmlFor="uname">Username</label></td><td><input type="text" name="uname" id="uname" required onChange={(event)=>{setUsername(event.target.value)}}/></td></tr>
+                        <tr><td><label htmlFor="pass">Password</label></td><td><input type="password" name="pass" id="pass" required onChange={(event)=>{setPassword(SHA512(event.target.value).toString())}}/></td></tr>
                         </tbody>
                     </table>
                     <input type="submit" value="Login" id="loginSubmit" />
