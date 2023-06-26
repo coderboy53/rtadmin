25420d1fc6fc6ef89de080b2170a5a625d20da9b
coderboy53 <mitra.soham53@gmail.com>
2023-06-26 17:39:20 +0530

    setup navbar, fixed authentication verification, added function for logout, set a button group for all the button

--- client/src/Hotel.jsx
+++ client/src/Hotel.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect, useRef } from 'react';
 import { useJwt } from 'react-jwt';
 import { Cookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
+import { RxDashboard } from "react-icons/rx"
+import { FiLogOut } from "react-icons/fi"
 const Hotel = () => {
     // states for input enabled checking and hotel form data storing
     const [enabled, setEnabled] = useState(false);
@@ -15,58 +17,54 @@ const Hotel = () => {
     const cookies = new Cookies;
     let token = cookies.get('jwt');
     let isJwtSet = token !== undefined;
-    const { decodedToken, isExpired } = useJwt(token);
+    const { isExpired } = useJwt(token);
 
     // handling addition of data as object from hotel form
     const handleChange = (event) => {
         let name = event.target.name;
         const entry = {};
         entry[name] = event.target.value;
-        setHotel({...hotel, ...entry});
+        setHotel({ ...hotel, ...entry });
     }
     // handles form submit, added so that i can keep both native form validation as well as add my custom submit handling
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if(event.nativeEvent.submitter.value === 'add')
-        {
+        if (event.nativeEvent.submitter.value === 'add') {
             handleAdd();
         }
-        else if(event.nativeEvent.submitter.value === 'update')
-        {
+        else if (event.nativeEvent.submitter.value === 'update') {
             handleUpdate();
         }
     };
 
     //handling updating hotel details
     const handleUpdate = async () => {
-        try{
+        try {
             const response = await fetch('http://127.0.0.1:4242/api/hotel/update', {
                 method: 'PUT',
-                headers: {'Content-Type' : 'application/json'},
-                body: JSON.stringify(hotel)  
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(hotel)
             });
             const jsonData = await response.json();
             alert(jsonData.message);
         }
-        catch(error)
-        {
+        catch (error) {
             console.log(error);
         }
     };
 
     //handling adding hotel entry
     const handleAdd = async () => {
-        try{
+        try {
             const response = await fetch('http://127.0.0.1:4242/api/hotel/add', {
                 method: 'POST',
-                headers: {'Content-Type' : 'application/json'},
-                body: JSON.stringify(hotel)  
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(hotel)
             });
             const jsonData = await response.json();
             alert(jsonData.message);
         }
-        catch(error)
-        {
+        catch (error) {
             console.log(error);
         }
     };
@@ -77,65 +75,89 @@ const Hotel = () => {
             isJwtSet = false;
         }
     });
-    if(isJwtSet){
-        return (
-        <>
-            <h2>Add New Hotel</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="left">
-                    <label htmlFor="hName">Hotel Name:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="hName" id="hName" required onChange={handleChange}/><br />
-                    <label htmlFor="hadd">Hotel Address:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="hAdd" id="hAdd" required onChange={handleChange}/><br />
-                    <label htmlFor="city">City:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="city" id="city" required onChange={handleChange}/><br />
-                    <label htmlFor="state">State:</label>
-                    <input type="text" name="state" id="state" onChange={handleChange}/><br />
-                    <label htmlFor="latitude">Latitude:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="latitude" id="latitude" required onChange={handleChange}/><br />
-                    <label htmlFor="rCount">Room Count:<span style={{color:'red'}}>*</span></label>
-                    <input type="number" name="rCount" id="rCount" required onChange={handleChange}/><br />
-                </div>
-                <div className="right">
-                    <label htmlFor="country">Country:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="country" id="country" required onChange={handleChange}/><br />
-                    <label htmlFor="starRate">Star Rating:<span style={{color:'red'}}>*</span></label>
-                    <select name="starRate" id="starRate" required onChange={handleChange}>
-                        <option value="null" disabled selected>Select</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select><br />
-                    <label htmlFor="hSortName">Hotel Sort Name:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="hSortName" id="hSortName" required onChange={handleChange}/><br />
-                    <label htmlFor="zipCode">Zip Code:</label>
-                    <input type="text" name="zipCode" id="zipCode" pattern="[1-8][0-9]{5}" onChange={handleChange}/><br />
-                    <label htmlFor="telNo">Telephone No:</label>
-                    <input type="text" name="telNo" id="telNo" pattern="[1-9]{5}[0-9]{5}" onChange={handleChange}/><br />
-                    <label htmlFor="longitude">Longitude:<span style={{color:'red'}}>*</span></label>
-                    <input type="text" name="longitude" id="longitude" required onChange={handleChange}/><br />
-                </div>
-                <label htmlFor="propId">Property ID:<span style={{color:'red'}}>*</span></label><br /><br />
-                <input type="radio" name="radio" id="hCode" onChange={()=>{if(enabled) setEnabled(false)}}/>Same as hotel code <br />
-                <input type="radio" name="radio" id="value" onChange={()=>{setEnabled(!enabled)}}/>Value<span style={{color:'red'}}>*</span> <br />
-                {
-                    enabled? (
-                        <input type="text" name="propId" id="propId" size={50} required onChange={handleChange}/>
-                    ):
-                    (
-                        <input type="text" name="propId" id="propId" size={50} disabled style={{backgroundColor:'#bcbcbc'}}/>
-                    )
-                } <br />
-                <button value="update">Save and Close</button>
-                <button value="add">Save and Add</button>
-                <button type='reset'>Reset</button>
-            </form>
-       </> 
-    );
+
+    // for logout
+    const logout = () => {
+        cookies.remove('jwt');
+        isJwtSet = false;
+        navigate('/login')
     }
-    else{
+
+    //final component output
+    if (isJwtSet) {
+        return (
+            <>
+                <nav>
+                    <div id="title">
+                        <h1>RTAdmin</h1>
+                    </div>
+                    <div id="left">
+                        <button onClick={(e) => { navigate("/dashboard") }}>
+                            <RxDashboard /> To Dashboard
+                        </button>
+                        <button id="logout" onClick={logout}>Logout <FiLogOut /> </button>
+                    </div>
+                </nav>
+                <main id='hotelSection'>
+                    <h2>Add New Hotel</h2>
+                    <form id="hotelForm" onSubmit={handleSubmit}>
+                        <div className="left">
+                            <label htmlFor="hName">Hotel Name:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="hName" id="hName" required onChange={handleChange} /><br />
+                            <label htmlFor="hadd">Hotel Address:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="hAdd" id="hAdd" required onChange={handleChange} /><br />
+                            <label htmlFor="city">City:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="city" id="city" required onChange={handleChange} /><br />
+                            <label htmlFor="state">State:</label>
+                            <input type="text" name="state" id="state" onChange={handleChange} /><br />
+                            <label htmlFor="latitude">Latitude:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="latitude" id="latitude" required onChange={handleChange} /><br />
+                            <label htmlFor="rCount">Room Count:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="number" name="rCount" id="rCount" required onChange={handleChange} /><br />
+                        </div>
+                        <div className="right">
+                            <label htmlFor="country">Country:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="country" id="country" required onChange={handleChange} /><br />
+                            <label htmlFor="starRate">Star Rating:<span style={{ color: 'red' }}>*</span></label>
+                            <select name="starRate" id="starRate" required onChange={handleChange}>
+                                <option value="null" disabled selected>Select</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select><br />
+                            <label htmlFor="hSortName">Hotel Sort Name:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="hSortName" id="hSortName" required onChange={handleChange} /><br />
+                            <label htmlFor="zipCode">Zip Code:</label>
+                            <input type="text" name="zipCode" id="zipCode" pattern="[1-8][0-9]{5}" onChange={handleChange} /><br />
+                            <label htmlFor="telNo">Telephone No:</label>
+                            <input type="text" name="telNo" id="telNo" pattern="[0-9][1-9][1-9][1-9][0-9]{3}[0-9]{4}" onChange={handleChange} /><br />
+                            <label htmlFor="longitude">Longitude:<span style={{ color: 'red' }}>*</span></label>
+                            <input type="text" name="longitude" id="longitude" required onChange={handleChange} /><br /><br />
+                        </div>
+                        <label htmlFor="propId">Property ID:<span style={{ color: 'red' }}>*</span></label><br /><br />
+                        <input type="radio" name="radio" id="hCode" onChange={() => { if (enabled) setEnabled(false) }} />Same as hotel code <br />
+                        <input type="radio" name="radio" id="value" onChange={() => { setEnabled(!enabled) }} />Value<span style={{ color: 'red' }}>*</span> <br />
+                        {
+                            enabled ? (
+                                <input type="text" name="propId" id="propId" size={50} required onChange={handleChange} />
+                            ) :
+                                (
+                                    <input type="text" name="propId" id="propId" size={50} disabled style={{ backgroundColor: '#bcbcbc' }} />
+                                )
+                        } <br />
+                        <div id='buttonGroup'>
+                            <button value="update">Save and Close</button>
+                            <button value="add">Save and Add</button>
+                            <button type='reset'>Reset</button>
+                        </div>
+                    </form>
+                </main>
+            </>
+        );
+    }
+    else {
         navigate('/login'); //when token is expired, redirect back to login page
     }
 }
